6311e8c3065ba5b22f5a3fdba76565271d3b262f
phu-idnow <phu.nguyen@idnow.de>
2020-03-16 15:11:54 +0700

    Add source

---
+++ iOS_Basic_Jenkins/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_Basic_Jenkins
+//
+//  Created by Phu Nguyen on 3/16/20.
+//  Copyright © 2020 The Vis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
